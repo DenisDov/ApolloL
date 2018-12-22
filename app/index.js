266b4de35b04853fbd45fa15fefc199eeb20a247
Denis Dovzhenko <dovzdenis@gmail.com>
2018-12-22 16:08:33 +0200

    new endpoint

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import FlashMessage from 'react-native-flash-message';
 
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -8,7 +9,7 @@ import { ApolloProvider } from 'react-apollo';
 import AppNavigator from './navigation/AppNavigator';
 
 const client = new ApolloClient({
-  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
+  uri: 'https://api.graph.cool/simple/v1/cji1nnnyw8hia0115y4ro03xr',
 });
 
 const AppContainer = createAppContainer(AppNavigator);
@@ -20,6 +21,7 @@ export default class App extends Component {
     return (
       <ApolloProvider client={client}>
         <AppContainer />
+        <FlashMessage icon="auto" duration={300} />
       </ApolloProvider>
     );
   }
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import {
+  View, Text, FlatList, Image,
+} from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Touchable from 'react-native-platform-touchable';
+import { showMessage } from 'react-native-flash-message';
 
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import { LoaderInset } from '../components/Loader';
 
+const styles = EStyleSheet.create({
+  post: {
+    padding: 16,
+    backgroundColor: 'white',
+    borderWidth: 0.5,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    margin: 16,
+    marginBottom: 0,
+    '@media ios': {
+      shadowColor: 'rgba(0,0,0, .4)',
+      shadowOffset: { height: 1, width: 1 },
+      shadowOpacity: 0.5,
+      shadowRadius: 1,
+    },
+    '@media android': {
+      elevation: 2,
+    },
+  },
+});
+
 const GET_RATES = gql`
   {
-    rates(currency: "USD") {
-      currency
-      rate
+    allPosts {
+      id
+      date
+      title
+      imgUrl
+      description
     }
   }
 `;
@@ -20,21 +49,37 @@ class HomeScreen extends React.Component {
     title: 'Home',
   };
 
+  _handleRefresh = async (callback) => {
+    await callback();
+    await showMessage({
+      message: 'Stories updated',
+      type: 'success',
+    });
+  };
+
   ExchangeRates = () => (
     <Query query={GET_RATES}>
-      {({ loading, error, data }) => {
+      {({
+        loading, error, data, refetch,
+      }) => {
         console.log('data: ', data);
         if (loading) return <LoaderInset />;
-        if (error) return <Text>Error :(</Text>;
+        if (error) return <Text>Something was wrong...</Text>;
         return (
           <FlatList
-            data={data.rates}
+            data={data.allPosts}
+            onRefresh={() => this._handleRefresh(refetch)}
+            refreshing={loading}
             renderItem={({ item }) => (
-              <View style={{ paddingHorizontal: 16, paddingVertical: 8 }}>
-                <Text>{`${item.currency}: ${item.rate}`}</Text>
-              </View>
+              <Touchable style={styles.post}>
+                <View>
+                  <Text style={{ fontSize: 12, color: '#617886' }}>{item.date}</Text>
+                  <Image source={{ uri: item.imgUrl }} style={{ height: 200 }} resizeMode="cover" />
+                  <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{item.title}</Text>
+                </View>
+              </Touchable>
             )}
-            keyExtractor={item => item.currency}
+            keyExtractor={item => item.id}
           />
         );
       }}
